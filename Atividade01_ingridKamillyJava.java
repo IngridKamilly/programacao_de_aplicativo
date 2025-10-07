c8e807d09e9e6ad77bcdfee3ce71b117cdf26fcf
Ingrid_Kamilly <0001140289@senaimgaluno.com.br>
2025-10-07 20:14:43 -0300

    Add files via upload

---
+++ Atividade01_ingridKamillyJava.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atividade01_ingridkamilly.java;
+
+/**
+ *
+ * @author ingri
+ */
+public class Atividade01_ingridKamillyJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double a = 25,b = a * 3,c = 10,d = a/c;
+        
+        System.out.println("O valor de A é: " +a);
+        System.out.println("O valor de B é: " +b);
+        System.out.println("O valor de C é: " +c);
+        System.out.println("O valor de D é: " +d);
+        
+        // TODO code application logic here
+    }
+    
+}
